6ae6780287a6d5a6a9c152522595fe81d967d975
Alyssa Falcione <lyzarddz@gmail.com>
2023-02-07 18:10:59 -0500

    added associations to pet model

--- app/models/adopter.rb
+++ app/models/adopter.rb
@@ -3,4 +3,6 @@ class Adopter < ApplicationRecord
     has_many :pets
     has_many :rescues, through: :pets
     has_many :summaries, through: :pets
+
+    has_secure_password
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,6 @@
 class Pet < ApplicationRecord
   belongs_to :rescue
   belongs_to :adopters
+
+  has_many :summaries
 end
