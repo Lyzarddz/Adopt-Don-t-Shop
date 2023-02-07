a1f6b4fa93acca14931b3a7ae7e584b2edb79156
Alyssa Falcione <lyzarddz@gmail.com>
2023-02-07 18:38:00 -0500

    added scope to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
+  scope :api do
   resources :summaries
   resources :adopters
   resources :rescues
   resources :pets
+
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
