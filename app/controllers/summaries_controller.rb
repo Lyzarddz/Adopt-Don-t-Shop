fbdbd58bfca2fb59b1c67050bc80ae5de13a8f23
Alyssa Falcione <lyzarddz@gmail.com>
2023-02-07 19:19:13 -0500

    added summary controller logic

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -1,2 +1,33 @@
-class SummariesController < ApplicationController
+class Api::SummariesController < ApplicationController
+
+    skip_before_action :authorize, only: [:create]
+    
+    def index 
+        render json: Summary.all, status: :ok
+    end
+
+    def show
+        summary = Summary.find(params[:id])
+        render json: summary
+    end 
+
+    def create
+        pet = Summary.pets.find(params[:pet_id])
+        summary = pet.summary.create!(summary_params)
+        render json: record, status: :created
+    end
+
+    def update
+        summary = Summary.find(params[:id])
+        summary.update!(summary_params)
+        render json: summary, status: :created
+    end
+
+    
+    def destroy
+        summary = current_adopter.summaries.find(params[:id])
+        summary.destroy
+        head :no_content
+    end
+    
 end
