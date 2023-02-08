4e7533974f09db9117b7f461d5ae1a1af2313abb
Alyssa Falcione <lyzarddz@gmail.com>
2023-02-07 19:17:23 -0500

    added adopter controller logic

--- app/controllers/adopters_controller.rb
+++ app/controllers/adopters_controller.rb
@@ -1,2 +1,21 @@
-class AdoptersController < ApplicationController
+class Api::AdoptersController < ApplicationController
+
+    skip_before_action :authorize, only: :create
+
+    def create 
+    adopter = Adopter.create!(adopter_params),
+    session[:adopter_id] = adopter.id
+    render json: adopter, status: :created
+    end
+
+    def show 
+        render json: Adopter.all, status: :ok
+    end
+
+    private_methods
+
+    def adopter_params
+        params.require(:adopter).permit( :name, :password, :email)
+    end
+
 end
