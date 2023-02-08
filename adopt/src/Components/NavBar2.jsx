5b7a2f3580c4e1cc482c9314f6cf457d6c26ac39
Alyssa Falcione <lyzarddz@gmail.com>
2023-02-07 23:25:34 -0500

    added styling to navbar

--- adopt/src/Components/NavBar2.jsx
+++ adopt/src/Components/NavBar2.jsx
@@ -3,6 +3,8 @@ import { Link, useNavigate } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import React, { useState } from "react";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -10,24 +12,57 @@ const useStyles = makeStyles((theme) => ({
       flexGrow: 1,
     },
     menuButton: {
-      marginRight: theme.spacing(2),
+        paddingRight: 25,
+        minWidth: 150,
+        minHeight: 60
     },
     title: {
       flexGrow: 1,
-      paddingLeft: 10,
+      padding: 150,
   
     },
     green: {
-      backgroundColor: "#CECDD2"
+      backgroundColor: "#aab4bb",
+
     },
     heights:{
         height:130
+    },
+    second: {
+        textAlign: "left"
     }
   }));
 
 function NavBar2(){
 
       const classes = useStyles();
+      const [currentUser, setCurrentUser] = useState(true);
+
+
+  function loggedInLinks(){
+    return (
+      <div >
+              <Button color="inherit"  variant="outlined" to="/" className={classes.menuButton}  component={ Link }>Home</Button> 
+              &nbsp;
+              &nbsp;
+              <Button color="inherit"  variant="outlined" to="/pets" className={classes.menuButton}  component={ Link }>Adoptable Pets</Button> 
+              &nbsp; 
+              &nbsp;
+              <Button color="inherit"  variant="outlined" to="/profile" className={classes.menuButton}  component={ Link }>My Profile</Button> 
+              &nbsp;
+              &nbsp;
+      </div>
+    )
+  }
+
+  function loggedOutLinks(){
+    return(
+      <div >
+          <Button color="inherit" to="/" component={ Link }>Home</Button> 
+          <Button color="inherit" to="/pets" component={ Link }>Adoptable Pets</Button> 
+      </div>
+    )
+  }
 
     return (
         <div className={classes.root}>
@@ -36,6 +71,7 @@ function NavBar2(){
             <Typography variant="h6" className={classes.title}>
         
             </Typography>
+            { currentUser ? loggedInLinks() : loggedOutLinks()}
           </Toolbar>
         </AppBar>
       </div>
