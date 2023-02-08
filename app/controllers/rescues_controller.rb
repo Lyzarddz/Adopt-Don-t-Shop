ec40459dacb81c46ef330b6cf768ddb0b2489576
Alyssa Falcione <lyzarddz@gmail.com>
2023-02-07 19:18:43 -0500

    added rescues controller logic

--- app/controllers/rescues_controller.rb
+++ app/controllers/rescues_controller.rb
@@ -1,2 +1,21 @@
-class RescuesController < ApplicationController
+class Api::RescuesController < ApplicationController
+
+    skip_before_action :authorize, only: :create
+
+    def create 
+       rescues = Rescue.create!(rescues_params),
+       session[:rescues_id] = rescues.id
+       render json: rescues, status: :created
+    end
+    
+    def show 
+        render json: @current_rescues, status: :ok
+    end
+    
+    private_methods
+    
+    def rescues_params
+        params.require(:rescue).permit( :name, :password, :email)
+    end
+    
 end
