520c05f1fddf0c052caa1e44e0207bd7a085840d
Alyssa Falcione <lyzarddz@gmail.com>
2023-02-07 19:20:46 -0500

    added session controller logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+
+    skip_before_action :authorize, only: :create
+
+    def create 
+        adopter = Adopter.find_by(name: params[:name])
+        if adopter&.authenticate(params[:password])
+            session[:adopter_id] = adopter.id  
+            render json: adopter,  status: :ok
+        else
+            render json: {errors:["Invalid username or password"]}, status: :unauthorized
+        end
+    end
+
+    def destroy
+        session.delete :owner_id
+        render json: @current_adopter
+    end
+    
 end
