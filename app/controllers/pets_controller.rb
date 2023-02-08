9a1f88614fa7a2060ea9de463dc591240c8723c8
Alyssa Falcione <lyzarddz@gmail.com>
2023-02-07 19:18:06 -0500

    added logic to pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,29 @@
-class PetsController < ApplicationController
+class Api::PetsController < ApplicationController
+
+    def index 
+        render json: current_adopter.pets, status: :ok
+    end
+
+    def show 
+        pet = current_adopter.pets.find(params[:id])
+        render json: pet
+    end
+
+    def create
+        pet = current_adopter.pets.create!(pet_params)
+        render json: pet, status: :created
+    end
+
+    def destroy
+        pet = current_adopter.pets.find(params[:id])  
+        pet.destroy 
+        head :no_content 
+    end
+
+    private
+
+    def pet_params
+        params.permit(:name, :gender, :age, :friendly, :house_trained, :health , :description, :adopters_id, :rescues_id)
+    end
+
 end
