60d97996bf742d4241c4fbf764b1c0e42f990f2f
Alyssa Falcione <lyzarddz@gmail.com>
2023-02-12 21:52:14 -0500

    added pet data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,29 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-ssp = Rescue.create!(name: "Stray Salvation Project", email: "ssp@gmail.com", phone: "412-646-7899", location: "West Mifflin", image:"/309350839_130865063030495_6512279244326944800_n.png")
+# ssp = Rescue.create!(name: "Stray Salvation Project", email: "ssp@gmail.com", phone: "412-646-7899", location: "West Mifflin", image:"/309350839_130865063030495_6512279244326944800_n.png")
 
-jbh = Rescue.create!(name: "Blood's Hound Rescue", email: "jbh@gmail.com", phone: "412-646-7856", location: "Munhall", image:"/images.jpeg")
+# jbh = Rescue.create!(name: "Blood's Hound Rescue", email: "jbh@gmail.com", phone: "412-646-7856", location: "Munhall", image:"/images.jpeg")
 
-
-
-
+# pap = Rescue.create!(name: "Paws Across Pittsburgh", email: "pap@gmail.com", phone: "412-564-7856", location: "Pittsburgh", image:"/images/326262498_593141575977135_2479792726838768400_n.jpg")
 
 
+# pap = Rescue.create!(name: "Sable Kennel", email: "sk@gmail.com", phone: "412-564-3246", location: "Pittsburgh", image:"/images/15776882_10211431756009662_773580057436209070_o.jpg")
 
-#   mrBeefy = Pet.create!(name:"Mr Beefy", age: 2, gender: "Male", breed: "Bulldog", friendly:"Yes", house_tra
-#     ined:"Yes", health:"Good", description: "Sweet", rescue_id: 2,  image:"/images/316810872_448378457485203_1916068418454027368_n.jpg")
 
-#     mrBeefy = Pet.create!(name:"Mr Beefy", age: 2, gender: "Male", breed: "Bulldog", friendly:"Yes", house_tra
-#         ined:"Yes", health:"Good", description: "Sweet", rescue_id: 1,  image:"/images/316810872_448378457485203_1916068418454027368_n.jpg")
+
+#   mrBeefy = Pet.create!(name:"Mr Beefy", age: 2, gender: "Male", breed: "Bulldog", friendly:"Yes", house_trained:"Yes", health:"Good", description: "Sweet", rescue_id: 2,  image:"/images/316810872_448378457485203_1916068418454027368_n.jpg", adopter_id:21)
+
+# ducky = Pet.create!(name:"Ducky", age: 1, gender: "Male", breed: "Lab", friendly:"Yes", house_trained:"Yes", health:"Good", description: "Energetic. Loves playing with other animals", rescue_id: 6,  image:"/images/318758827_459068106416238_2796414441513115284_n.jpg", adopter_id:21)
+
+# lucy = Pet.create!(name:"Lucy", age: 4, gender: "female", breed: "Pit Mix", friendly:"Yes", house_trained:"Yes", health:"Good", description: "Timid at first but warms up quickly. Loves to snuggle and lounge", rescue_id: 1,  image:"images/321332803_708642347318729_8831414895173487154_n.jpg", adopter_id:21)
+
+# lady = Pet.create!(name:"Lady", age: 13, gender: "Female", breed: "Papillon", friendly:"Yes", house_trained:"No", health:"Partially Blind", description: "Cuddle bug", rescue_id: 5,  image:"/images/323062194_698828608435010_3472967698685077704_n.jpg", adopter_id:21)
+
+# biscuit = Pet.create!(name:"Biscuit", age: 13, gender: "Female", breed: "French Bulldog", friendly:"Yes", house_trained:"Yes", health:"Good", description: "Shy with new people", rescue_id: 2,  image:"/images/327212051_881645349550803_4319022790855665102_n.jpg", adopter_id:21)
+
+# mauve = Pet.create!(name:"Mauve", age: 13, gender: "Female", breed: "Shepherd Mix", friendly:"Yes", house_trained:"Yes", health:"Good", description: "Fun loving and obedient", rescue_id: 4,  image:"/images/328086298_1364653967615697_8210005094934146252_n.jpg", adopter_id:21)
+
+# jack = Pet.create!(name:"Jack", age: 1, gender: "Male", breed: "Lab Mix", friendly:"Yes", house_trained:"No", health:"Good", description: "Fun loving and obedient", rescue_id: 4,  image:"/images/328350756_3255829908000680_5246061057846847_n.jpg", adopter_id:21)
+
+jack = Pet.create!(name:"Jack", age: 1, gender: "Male", breed: "Lab Mix", friendly:"Yes", house_trained:"No", health:"Good", description: "Fun loving and obedient", rescue_id: 4,  image:"/images/328931221_737371347777913_3949598533835669212_n.jpg", adopter_id:21)
